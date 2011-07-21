f8aa0725b925cc0f229c7648bb38358cbef671b5
jconley <conley.jj@gmail.com>
2011-07-20 22:48:24 -0400

    Initial Commit?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupTrip::Application.config.secret_token = '8f138ef184dbfa8e827e5ef29e41bc3adc25b2f124bb7af3e9cd650af24994b386f0f6946f49b5cdd7d8487bdd75a182c13e7e701ddb241d2fd3451c8419745f'
